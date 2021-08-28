979ce646aeeb4bb6408a0a66bf669750999dce93
unknown <abhi.bajpai326@gmail.com>
2021-08-29 01:32:36 +0530

    Second Commit 1:32 AM

--- src/main/java/com/admin/controllers/AdminFLightListController.java
+++ src/main/java/com/admin/controllers/AdminFLightListController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,14 +54,28 @@ public class AdminFLightListController {
 	
 	//Deleting Flight By Id
 	
-	@DeleteMapping("flights/{id}")
+	@DeleteMapping("/flights/{id}")
 	public void deleteFlight(@PathVariable("id") int id) {
 		ResponseEntity<Void> deleteFlightById = template.exchange(
-				"",
+				"http://localhost:9092/flights"+"/"+"{id}",
 		          HttpMethod.DELETE,
 		          null,
 		          void.class,
 		          Integer.toString(id)
 				);
+	}
+	
+//	Block The Flight
+	
+	@PutMapping("/flights/{id}")
+	public FlightList blockFlight(@PathVariable("id") int id) {
+		ResponseEntity<FlightList> blockUnblock = template.exchange(
+				"http://localhost:9092/flights"+"/"+"{id}",
+		          HttpMethod.DELETE,
+		          null,
+		          FlightList.class,
+		          Integer.toString(id)
+				);
+		return blockUnblock.getBody();
 	}
 }
--- src/main/java/com/admin/entity/FlightList.java
+++ src/main/java/com/admin/entity/FlightList.java
@@ -10,12 +10,13 @@ public class FlightList {
 	private String arrivalTime;
 	private int totalSeats;
 	private int ticketCost;
+	private boolean block = false;
 	public FlightList() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	public FlightList(int airlineId, String airlineName, String fromLocation, String toLocation, String departureTime,
-			String arrivalTime, int totalSeats, int ticketCost) {
+			String arrivalTime, int totalSeats, int ticketCost, boolean block) {
 		super();
 		this.airlineId = airlineId;
 		this.airlineName = airlineName;
@@ -25,6 +26,7 @@ public class FlightList {
 		this.arrivalTime = arrivalTime;
 		this.totalSeats = totalSeats;
 		this.ticketCost = ticketCost;
+		this.block=block;
 	}
 	public int getAirlineId() {
 		return airlineId;
@@ -53,7 +55,7 @@ public class FlightList {
 	public String getDepartureTime() {
 		return departureTime;
 	}
-	public void setDepartureTime(String departureTime) {
+	public void setDepartureTime(String departureTime){
 		this.departureTime = departureTime;
 	}
 	public String getArrivalTime() {
@@ -73,6 +75,12 @@ public class FlightList {
 	}
 	public void setTicketCost(int ticketCost) {
 		this.ticketCost = ticketCost;
+	}
+	public boolean isBlock() {
+		return block;
+	}
+	public void setBlock(boolean block) {
+		this.block = block;
 	}
 	
 	
